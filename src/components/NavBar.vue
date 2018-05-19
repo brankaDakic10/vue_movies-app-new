00c77d210d17e5a57988de5c6525805acf67be93
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-19 10:08:53 +0200

    Added 1.Task

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark nav-style">
+            <router-link class="navbar-brand" :to="{name: 'home'}">CinemaApp</router-link>
+            <div class="navbar-nav">
+                <router-link class="nav-item nav-link" :to="{name: 'movies'}">Movies</router-link>
+
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .nav-style {
+        background-color: chocolate;
+    }
+</style>
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+
+import AppMovies from './components/AppMovies.vue'
+
+Vue.use(VueRouter)
+
+
+const routes = [
+    { path: '/', redirect: '/movies', name: 'home' },
+    { path: '/movies', component: AppMovies, name: 'movies' },
+   
+   
+  ]
+  
+  
+  export const router = new VueRouter({
+    mode: 'history',
+    routes
+  })
