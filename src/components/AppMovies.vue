1d6d6f46e8ea52585bc17cbc7196e4c4ddda4f97
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-19 11:05:30 +0200

    Added 4.Task

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -2,13 +2,24 @@
     <div class="container">
        <h3>List of Movies</h3>
        
+        <movie-row v-for="movie in movies" :key="movie.id" :movie="movie"></movie-row>
     </div>
 </template>
 
 <script>
     import MoviesService from './../services/MoviesService'
+    import MovieRow from './MovieRow.vue'
+
     export default {
         name: 'AppMovies',
+        components:{
+            MovieRow
+        },
+        data(){
+            return{
+                movies:[]
+            }
+        },
 
         beforeRouteEnter(to, from, next) {
 
