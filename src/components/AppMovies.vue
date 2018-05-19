0e51f3ae448892bfd90390863828006f1f445113
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-19 16:40:14 +0200

    Added 7.Task

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="container">
-       <h3>List of Movies</h3>
-        <movie-search
-      @search-term-change="onSearchTermChanged"
-      />
+        <h3>List of Movies</h3>
+        <div v-if="selectedMoviesIds.length">Selected: {{ selectedMoviesCounter }}</div>
+        <movie-search @search-term-change="onSearchTermChanged" />
         <section class="container">
-        <movie-row v-for="movie in movies" :key="movie.id" :movie="movie"></movie-row>
-       <div v-if="!movies.length">
-        No Movies
-       </div>
+            <movie-row v-for="movie in movies" 
+            :key="movie.id" :movie="movie" 
+            @on-selected-movie="onSelectedMovie"></movie-row>
+            <div v-if="!movies.length">
+                No Movies
+            </div>
         </section>
     </div>
 </template>
@@ -16,28 +17,43 @@
 <script>
     import MoviesService from './../services/MoviesService'
     import MovieRow from './MovieRow.vue'
-    import MovieSearch from './MovieSearch.vue' 
+    import MovieSearch from './MovieSearch.vue'
     export default {
         name: 'AppMovies',
-        components:{
+        components: {
             MovieRow,
             MovieSearch
         },
-        data(){
-            return{
-                movies:[]
+        data() {
+            return {
+                movies: [],
+                selectedMoviesIds: []
             }
         },
-        methods:{
-          onSearchTermChanged(term){
-              MoviesService.index(term)
-              .then(({ data }) => {
-               this.movies = data
-              })
-          }
+        methods: {
+            onSearchTermChanged(term) {
+                MoviesService.index(term)
+                    .then(({
+                        data
+                    }) => {
+                        this.movies = data
+                    })
+            },
+            onSelectedMovie(movie) {
+                if (this.selectedMoviesIds.indexOf(movie.id) > -1) {
+                    return;
+                }
+                this.selectedMoviesIds.push(movie.id)
+            }
 
 
         },
+        computed: {
+            selectedMoviesCounter() {
+      return this.selectedMoviesIds.length
+     }
+        },
+
 
         beforeRouteEnter(to, from, next) {
 
