9708ed28ba3778ca8b5620aafe5a086b94f99c5f
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-26 17:01:05 +0200

    Added changes in 6.Task- Assignment 2

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -1,58 +1,65 @@
 <template>
     <div class="container">
         <div class="col-8">
-         <b-form @submit.prevent="login">
+            <b-form @submit.prevent="login">
 
-            <b-form-group label="Email:">
-                <b-form-input type="email" v-model="email" placeholder="Enter email">
-                </b-form-input>
-            </b-form-group>
-            <b-form-group label="Password:">
-                <b-form-input type="password" v-model="password" placeholder="Enter password">
-                </b-form-input>
-            </b-form-group>
-               <!-- error block -->
-            <div v-for="(error, key) in errors" :key="key" v-if="error" class="alert alert-danger">
-              {{error}}
-            </div>
-            <!--  -->
-            
+                <b-form-group label="Email:">
+                    <b-form-input type="email" v-model="email" placeholder="Enter email">
+                    </b-form-input>
+                </b-form-group>
+                <b-form-group label="Password:">
+                    <b-form-input type="password" v-model="password" placeholder="Enter password">
+                    </b-form-input>
+                </b-form-group>
+                <!-- error block -->
+                <div v-for="(error, key) in errors" :key="key" v-if="error" class="alert alert-danger">
+                    {{error}}
+                </div>
+                <!--  -->
 
-            <b-button type="submit" variant="success">Submit</b-button>
 
-        </b-form>
+                <b-button type="submit" variant="success">Submit</b-button>
 
-</div>
+            </b-form>
+
+        </div>
 
     </div>
 </template>
 
 <script>
-import { authService } from '../services/AuthService'
+    import {
+        authService
+    } from '../services/AuthService'
 
-export default {
-  name: 'AppLogin',
-   data() {
+    export default {
+        name: 'AppLogin',
+        data() {
             return {
-                    errors:[],
-                    email: '',
-                    password: ''
-            
+                errors: [],
+                email: '',
+                password: '',
             }
         },
-     methods: {
-        login() {
-          authService.login(this.email,this.password)
-          .then(()=>{
-              this.$router.push({name:'movies'})
-          }).catch(errors => {
+        methods: {
+            login() {
+                authService.login(this.email, this.password)
+                    .then(() => {
+                        this.$router.push({
+                            name: 'movies'
+                        })
+
+                    }).catch(errors => {
                         this.errors = errors.response.data;
                     })
-      }
-       }
-}
+
+
+
+
+            }
+        }
+    }
 </script>
 
 <style>
-
 </style>
